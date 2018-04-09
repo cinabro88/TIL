106277c5c82a6626fede6262d0a64dc046d86e7b
시나브로 <cinabro88@gmail.com>
2018-04-09 22:25:46 +0900

    [인프런vueJS] 폼뷰에 입력값 여부에 따른 reset 버튼 노출처리 및 엔터이벤트 처리

--- vuejs/inflearn-vuejs/1-vanilla/js/views/FormView.js
+++ vuejs/inflearn-vuejs/1-vanilla/js/views/FormView.js
@@ -9,10 +9,24 @@ FormView.setup = function (el) {
     this.inputEl = el.querySelector('[type=text]')
     this.resetEl = el.querySelector('[type=reset]')
     this.showResetBtn(false)
+    this.bindEvents()
+    return this
 }
 
 FormView.showResetBtn = function(show = true) {
     this.resetEl.style.display = show ? 'block' : 'none'
+}
+
+FormView.bindEvents = function() {
+    this.on('submit', e => e.preventDefault())
+    this.inputEl.addEventListener('keyup', e => this.onKeyup(e))
+}
+
+FormView.onKeyup = function(e) {
+    const enter = 13
+    this.showResetBtn(this.inputEl.value.length)
+    if (e.keyCode !== enter) return
+    this.emit('@submit', {input: this.inputEl.value})
 }
 
 export default FormView
