7f888b3b963f5c0ae81937bd094b4b672611ba3a
시나브로 <cinabro88@gmail.com>
2018-04-16 22:12:49 +0900

    [인프런vueJS] 결과뷰 생성

--- vuejs/inflearn-vuejs/1-vanilla/js/controllers/MainController.js
+++ vuejs/inflearn-vuejs/1-vanilla/js/controllers/MainController.js
@@ -1,4 +1,6 @@
 import FormView from '../views/FormView.js'
+import ResultView from '../views/ResultView.js'
+import SearchModel from '../models/SearchModel.js'
 
 const tag = '[MainController]'
 
@@ -8,13 +10,27 @@ export default {
         FormView.setup(document.querySelector('form'))
         .on('@submit', e => this.onSubmit(e.detail.input))
         .on('@reset', e => this.clearResult())
+
+        ResultView.setup(document.querySelector('#search-result'))
+    },
+
+    search(query) {
+        console.log(tag, 'search()', query)
+        SearchModel.list(query).then(data => {
+            this.onSearachResult(data)
+        })
     },
 
     clearResult() {
-        console.log(tag, 'clearResult()')  
+        console.log(tag, 'clearResult()')
     },
 
     onSubmit(input) {
         console.log(tag, 'onSubmit()', input)
+        this.search(input)
+    },
+
+    onSearachResult(data) {
+        ResultView.render(data)
     }
 }
